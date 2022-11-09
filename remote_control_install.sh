0aad21477fd73b89055c2c3934df674e65b9eea6
publicuser3423 <84035173+publicuser3423@users.noreply.github.com>
2022-11-09 19:46:45 +0300

    Update remote_control_install.sh

--- remote_control_install.sh
+++ remote_control_install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 # sudo sh remote_control_install.sh
-# wget http://sitehere.com/install.sh && sudo sh remote_control_install.sh
+# wget https://raw.githubusercontent.com/publicuser3423/test/main/remote_control_install.sh && sudo sh remote_control_install.sh
 # аналог TeamViewer
 
 wget https://download.anydesk.com/linux/anydesk_6.2.1-1_amd64.deb
 dpkg -i anydesk_6.2.1-1_amd64.deb
 # установка требуемых зависимостей
-apt install -f
+# silent mode auto yes
+apt install -fy
 pause
